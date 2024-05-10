065e5b10d56a1c031f59196806b13c308c6b7e09
nfu-csie-qiushawa <qiushaua@gmail.com>
2024-05-10 16:26:44 +0800

    修正ping值單位

--- commands/Ping/__init__.py
+++ commands/Ping/__init__.py
@@ -8,7 +8,7 @@ class Ping(commands.Cog):
         self.bot = bot
     @app_commands.command(name = "查看延遲", description = "此命令可以查看機器人延遲時間")
     async def ping_command(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"機器人ping值: {round(self.bot.latency, 1)}ms", ephemeral=True)
+        await interaction.response.send_message(f"機器人ping值: {round(self.bot.latency* 1000, 1) }ms", ephemeral=True)
 async def setup(bot:commands.Bot):
     print(f"load: {Ping.__name__}")
     await bot.add_cog(Ping(bot))
