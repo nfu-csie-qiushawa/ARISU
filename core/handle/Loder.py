2574dd262aa627aab8c19f6cdc3a269417483ac1
nfu-csie-qiushawa <qiushaua@gmail.com>
2024-05-12 17:39:52 +0800

    更改架構及預設模組

--- core/handle/Loder.py
+++ core/handle/Loder.py
@@ -1,14 +1,28 @@
 import discord, os
 import asyncio
+import zipfile
 from discord.ext import commands
-__command=os.listdir("./core/commands")
-__events=os.listdir("./core/events")
-async def mods_loder():
-    ...
-async def commands_loder(bot:commands.Bot):
-    for extension in __command:
+__mods=[_ for _ in os.listdir("./mods") if _.endswith('.zip')]
+async def loder(bot:commands.Bot):
+    for file in __mods:
+        if 'command' in file: url='./core/commands'
+        elif 'events' in file: url='./core/events'
+        else: return print("檔名格式錯誤")
+        path=f'mods/{file}'
+        if not zipfile.is_zipfile(path): return print("檔案毀損")
+        ZIP = zipfile.ZipFile(path)
+        modname=file.replace("_command", "").replace("_event", "").replace(".zip", "")
+        try:
+            if modname not in os.listdir(url):ZIP.extractall(url,pwd=f"{os.getenv('BOT_ID')}".encode("utf-8"))
+        except:
+            print("你缺少權限")
+        ZIP.close()
+    await commands_loder(bot, os.listdir("./core/commands"))
+    await event_loder(bot, os.listdir("./core/events"))
+async def commands_loder(bot:commands.Bot,command):
+    for extension in command:
         await bot.load_extension(f'core.commands.{extension}')
         
-async def event_loder(bot:commands.Bot):
-    for extension in __events:
+async def event_loder(bot:commands.Bot, events):
+    for extension in events:
         await bot.load_extension(f'core.events.{extension}')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 import os
 from dotenv import load_dotenv
-from core.handle.Loder import commands_loder, event_loder
+from core.handle.Loder import loder
 load_dotenv()
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = "!", intents = intents)
@@ -13,8 +13,7 @@ async def main():
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
     logging.basicConfig(level=logging.DEBUG, filename='Log.log', filemode='a', format=FORMAT)
     async with bot:
-        await commands_loder(bot)
-        await event_loder(bot)
+        await loder(bot)
         await bot.start(os.getenv("DISCORD_TOKEN"))
 
 if __name__ == "__main__":
