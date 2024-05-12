dd743e9d0ca3fdd6e0dddf10dc9624835dfe32da
nfu-csie-qiushawa <qiushaua@gmail.com>
2024-05-12 17:50:34 +0800

    log

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+from datetime import datetime
 import logging
 import os
 from dotenv import load_dotenv
@@ -11,7 +12,7 @@ bot = commands.Bot(command_prefix = "!", intents = intents)
 
 async def main():
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-    logging.basicConfig(level=logging.DEBUG, filename='Log.log', filemode='a', format=FORMAT)
+    logging.basicConfig(level=logging.INFO, filename=f'logs/{datetime.today().strftime("%Y-%m-%d")}.log', filemode='a', format=FORMAT)
     async with bot:
         await loder(bot)
         await bot.start(os.getenv("DISCORD_TOKEN"))
